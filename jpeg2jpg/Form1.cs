09dcc466d9a6218bc77b1619e097490fa16f0134
Jeremiah Stillings <jstillings2@outlook.com>
2019-12-24 13:16:11 -0600

    Initial Commit

---
+++ jpeg2jpg/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security;
+using System.Diagnostics;
+
+namespace jpeg2jpg
+{
+    public partial class Form1 : Form
+    {
+        private Button selectButton;
+        private OpenFileDialog openFileDialog1;
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Basic copy paste from MS help site
+            // It creates a file open dialog off a button click to pick the .jpeg file
+
+            openFileDialog1 = new OpenFileDialog()
+            {
+                FileName = "Select a file",
+                Filter = "Picture files (*.jpeg)|*.jpeg",
+                Title = "Open .jpeg file"
+            };
+
+            selectButton = new Button()
+            {
+                Size = new Size(200, 100),
+                Location = new Point(300, 200),
+                Text = "Select file"
+            };
+            selectButton.Click += new EventHandler(selectButton_Click);
+            Controls.Add(selectButton);
+        }
+            private void selectButton_Click(object sender, EventArgs e)
+            {
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                try
+                {
+                    var filePath = openFileDialog1.FileName;
+                    string currentDirectory = Path.GetDirectoryName(filePath);
+                    using (Stream str = openFileDialog1.OpenFile())
+                    {
+                        System.Drawing.Image oldImage = System.Drawing.Image.FromFile(filePath);
+                        
+
+                        oldImage.Save(currentDirectory + "/new.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+                        System.Windows.Forms.MessageBox.Show("File Converted. It is called new.jpg in the folder that opens.");
+
+                        
+                    }
+
+                    Process.Start("explorer.exe", currentDirectory);
+
+                }
+                catch (SecurityException ex)
+                {
+                    MessageBox.Show($"Security error.\n\nError message: {ex.Message}\n\n" +
+                    $"Details:\n\n{ex.StackTrace}");
+                }
+                }
+          
+        }
+        }
+
+       
+       
+    }
+
---
+++ jpeg2jpg/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+// Slapped together by Jeremiah Stillings
+// This is to solve the issue of .JPEG not being accepted on some PPI uploads for work
+// 12/24/2019
+// jstillings2@outlook.com
+
+namespace jpeg2jpg
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
